322f6f58d8fe4a36a1ac4e0562be2accbef52ac1
David Buser <busdav@outlook.com>
2019-02-23 10:23:22 +0000

    Add route for userController#show

--- src/db/queries.users.js
+++ src/db/queries.users.js
@@ -1,5 +1,5 @@
 const User = require("./models").User;
-// const Post = require("./models").Post;
+const Wiki = require("./models").Wiki;
 // const Comment = require("./models").Comment;
 const bcrypt = require("bcryptjs");
 // const sgMail = require('@sendgrid/mail');
